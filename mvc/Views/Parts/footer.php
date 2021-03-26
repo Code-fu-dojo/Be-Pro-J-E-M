3e571d8f382b0acfe92e5a992a8b1b007b2db621
Enzo <e.meli@codeur.online>
2021-03-26 16:50:49 +0100

    d

--- mvc/Views/Parts/footer.php
+++ mvc/Views/Parts/footer.php
@@ -1,1 +1,43 @@
-footer
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<link rel="stylesheet" href="<?= WEBROOT ?>assets/styles/footer.css">
+
+<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./assets/images/image/index/logotype.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="./assets/images/image/index/logotype.png" class="d-block w-100" alt="...">
+    </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"  data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"  data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<div id="footer">
+<div class="container">
+  <div class="row align-items-start">
+    <div class="col-sm">
+      <img src="./assets/images/image/index/footer/logo-reseaux.png">
+    </div>
+    <div class="col-sm">
+        <!-- cadre newsletter -->
+<form action="">
+<fieldset>
+<legend id="marge">Subscribe to news</legend>
+<p><input type="email" name="" id=""><input type="submit"
+value="Submit"></p>
+</fieldset>
+</form>
+<!-- Fin cadre newsletter -->
+    </div>
+    <div class="col-sm" id="marge">
+    <h6 class="legend">CONTACT US<h6>
+      <p> <span class="legend">San Francisco, California <br> 400 Castro St, San Francisco, CA <br> (+1) 686-868-9999 </p>
+    <img src="./assets/images/image/index/footer/contactus-payment.png">
+    </div>
+</div>
