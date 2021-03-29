acce880887115d8e9019a3218007754588924288
JuliaH <j.humbert@codeur.online>
2021-03-29 14:27:42 +0200

    fluteLaPolice

--- mvc/Views/Products/index.php
+++ mvc/Views/Products/index.php
@@ -1,9 +1,9 @@
-<link href="<?= WEBROOT ?>assets/styles/index.css" rel="stylesheet">
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Playfair+Display&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-
+<link href="<?= WEBROOT ?>assets/styles/index.css" rel="stylesheet">
 <!-- CAROUSEL -->
 <div class="carousel-container container-fluid">
 <div id="carouselExampleControls" class="carousel slide" data-bs-interval="false" data-bs-ride="carousel">
