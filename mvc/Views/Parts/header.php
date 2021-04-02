fdfa22e29f4ee1f8e124b425531daa83af0de2a5
JuliaH <j.humbert@codeur.online>
2021-04-02 10:25:37 +0200

    css

--- mvc/Views/Parts/header.php
+++ mvc/Views/Parts/header.php
@@ -6,32 +6,48 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 <link href="<?= WEBROOT ?>assets/styles/index.css" rel="stylesheet">
 
-<div id="header" class="container-fluid">
-<div class="row">
+<!-- NAV -->
 
-<div class="col-sm">
-<nav class="nav">
-<a id="bepro" class="nav-link text-white" href="#">Be.Pro</a>
-</nav>
+<div id="header" class="container">
+  <div class="row">
+
+    <div class="col-3">
+      <div class="float-start">
+         <nav class="nav">
+              <a class="fs-4 fst-italic text-white playfair fw-bold nav-link" href="index.php">Be.Pro</a>
+        </nav>
+        </div>
+    </div>
+
+    <div class="col-2">
+    <div class="float-end">
+       <nav class="nav">
+               <a class="fs-6 text-white montserrat nav-link" href="index.php">HOME</a>
+                    </nav>
+       </div>
+       </div>
+       
+       <div class="col-2">
+         <div class="float-start">
+        <nav class="nav">
+              <a class= "fs-6 text-white montserrat nav-link" href="beproproduct.php">SHOP</a>
+       </nav>
+       </div>
+       </div>
+
+
+    <div class="col-5">
+    <div class="float-end">
+            <nav class="nav"> 
+                <a class="text-white nav-link" href="#">PANIER</a>
+            </nav>
+            </div>
+    </div>
+   
+
+  </div>
 </div>
 
-<div class="col-sm">
-<nav class="nav">
-<div class="row">
-<a id="home" class="nav-link text-white" href="index.php">HOME </a>
-</div>
-<div class="row">
-<a id="shop" class="nav-link text-white" href="../mvc/Views/Products/product_page.php">SHOP</a>
-</div>
-</nav>
-</div>
 
-<div class="col-sm">
-<nav class="nav">
-<a id="panier" class="nav-link text-white" href="#">PANIER</a>
-</nav>
-</div>
-
-</div>
-</div>
+<!-- FIN NAV -->
   
